9f31bc005bca9a7abcfe9ed95aa54444527ae292
Soren Frederiksen <soren.frederiksen@mindzie.com>
2025-07-25 18:51:59 -0400

    Added HelloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using CSnakes.Runtime;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+            var home = Path.Join(Environment.CurrentDirectory, "."); /* Path to your Python modules */
+            builder.Services
+                .WithPython()
+                .WithHome(home)
+                .FromRedistributable(); // Download Python 3.12 and store it locally
+
+            var app = builder.Build();
+
+            // Ensure the IPythonEnvironment interface is defined and the required package is referenced
+            IPythonEnvironment pythonEnv = app.Services.GetRequiredService<IPythonEnvironment>();
+            var msg = pythonEnv.Hello().HelloWorld("Soren");
+            Console.WriteLine(msg);
+        }
+    }
+}
